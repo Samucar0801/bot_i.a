cf3c94aa09a7bf68e196daea38bb03ff7caedd65
Samucar0801 <samuel.rodrigues0801@gmail.com>
2024-08-30 13:21:51 -0300

    add uma nova pergunta

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ class SimpleChatbot:
             'como você está?': ['Estou bem, obrigado!', 'Tudo certo, e você?'],
             'qual é o seu nome?': ['Eu sou um chatbot sem nome.', 'Você pode me chamar de Chatbot.'],
             'tchau': ['Tchau!', 'Até logo!', 'Adeus!'],
+            'explique oque é git pull':['comando para puxar do repositório remoto para o local'],
             'passa a resposta da lição': ['qual é a lição?'],
             'lição de ia': ['qual semana e aula?'],
             's15- aula- 3': ['a resposta é criar perguntas para o chat bot interagir com você']
